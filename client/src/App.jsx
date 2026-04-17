e003dff93871c6c28acda0bcdc4a72318044667b
kranthi7581 <kranthikumarakula7@gmail.com>
2026-04-17 11:33:18 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Welcome to the React App!</h1>
+    </>
+  )
+}
+
+export default App
